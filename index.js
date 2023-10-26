4a20eb8062bbf7494d5523dc64fcc4d20a94692c
GabyAM <gdgabyam@gmail.com>
2023-10-26 19:57:04 -0300

    Update index.js
    this had to go with the last commit.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const options = ["rock", "paper", "scissors"]
 
 function getComputerChoice() {
@@ -5,43 +6,61 @@ function getComputerChoice() {
     return item
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice()
+    const $computerChoice = document.querySelector('.computer-choice')
+    $computerChoice.textContent = computerSelection
+
     if(playerSelection === computerSelection) {
-        return 'draw'
+        return('draw')
     }
     if (
         (playerSelection === "rock" && computerSelection === "paper")
         | (playerSelection === 'paper' && computerSelection === 'scissors')
         | (playerSelection === 'scissors' && computerSelection === 'rock')
     ) {
-        return 'computer'
+        return ('computer')
     }
-    return 'player'
+    return ('player')
+}
+
+function updateResultText (text) {
+    const $result = document.querySelector('.result-text');
+    $result.textContent = text;
+}
+
+function updateScores() {
+    if(playerScore === 3 || computerScore === 3) {
+        const buttons = document.querySelectorAll('.input-button');
+        buttons.forEach(button => {button.disabled = true})
+    }
+    const $playerScore = document.querySelector('.player-score');
+    const $computerScore = document.querySelector('.computer-score');
+    $playerScore.textContent = `Player score: ${playerScore}`
+    $computerScore.textContent = `Computer score: ${computerScore}`
+    
 }
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    while(playerScore !== 3 && computerScore !== 3) {
-        const playerChoice = prompt('Select rock, paper or scissor: ', '').toLowerCase()
-        const computerChoice = getComputerChoice()
-        const winner = playRound(playerChoice, computerChoice)
-
-        if(winner === 'player') {
-            playerScore++
-            console.log(`You win! ${playerChoice} beats ${computerChoice}`)
-        } else if (winner ==='computer'){
-            computerScore++
-            console.log(`You lose! ${computerChoice} beats ${playerChoice}`)
-        } else console.log('draw!')
-
-        console.log('player: ' + playerScore + ', computer: ' + computerScore)
-    }
-
-    if(playerScore === 3) {
-        console.log('The player wins!')
-    } else console.log('The computer wins!')
+    const buttons = document.querySelectorAll('.input-button');
+    buttons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const playerChoice = event.target.textContent.toLowerCase()
+            const result = playRound(playerChoice)
+            if(result === 'draw') {
+                updateResultText('draw')
+            } else if (result === 'player') {
+                updateResultText ('the player wins the round!')
+                playerScore++;
+            } else {
+                updateResultText('the computer wins the round!')
+                computerScore++;
+            }
+            updateScores()
+        })
+    })
 }
 
+let playerScore = 0;
+let computerScore = 0;
 game();
