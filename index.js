f7e16ca4037cf71aae7a74b3f9ad34e56b6a112e
GabyAM <gdgabyam@gmail.com>
2023-10-26 14:46:17 -0300

    adds the rest of the logic
    i forgot to commit :)

--- index.js
+++ index.js
@@ -4,3 +4,44 @@ function getComputerChoice() {
     const item = options[Math.floor(Math.random()*options.length)]
     return item
 }
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        return 'draw'
+    }
+    if (
+        (playerSelection === "rock" && computerSelection === "paper")
+        | (playerSelection === 'paper' && computerSelection === 'scissors')
+        | (playerSelection === 'scissors' && computerSelection === 'rock')
+    ) {
+        return 'computer'
+    }
+    return 'player'
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    while(playerScore !== 3 && computerScore !== 3) {
+        const playerChoice = prompt('Select rock, paper or scissor: ', '').toLowerCase()
+        const computerChoice = getComputerChoice()
+        const winner = playRound(playerChoice, computerChoice)
+
+        if(winner === 'player') {
+            playerScore++
+            console.log(`You win! ${playerChoice} beats ${computerChoice}`)
+        } else if (winner ==='computer'){
+            computerScore++
+            console.log(`You lose! ${computerChoice} beats ${playerChoice}`)
+        } else console.log('draw!')
+
+        console.log('player: ' + playerScore + ', computer: ' + computerScore)
+    }
+
+    if(playerScore === 3) {
+        console.log('The player wins!')
+    } else console.log('The computer wins!')
+}
+
+game();
